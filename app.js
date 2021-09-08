83ede99488ef31dbb1f88e1dfdd46c61ff6c77ab
정성훈 <seonghun@jeongseonghun-ui-MacBookAir.local>
2021-09-08 22:22:42 +0900

    add static files

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const express = require("express");
 //express의 반환값을 저장한다.
 const app = express();
 
+app.use('/static', express.static('public'));
 
-app.get("/", (req, res) => {
-  res.send("스마트 해상물류-TinyML Team Blog");
-});
+
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + "/public/main.html")
+})
+
+
+
 
 //3000번 포트로 서버를 오픈한다.
 app.listen(3000, () => {
